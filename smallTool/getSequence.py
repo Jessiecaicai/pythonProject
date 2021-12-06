7bb1ba1d28a7ae50ac236169f0a5f3f0d053e8da
jessie <jessiecaicai@yeah.net>
2021-12-06 21:07:19 +0800

    12.6

--- smallTool/getSequence.py
+++ smallTool/getSequence.py
@@ -2,20 +2,21 @@
 # @Author  : Jessie
 # @Time    : 2021/11/17 3:58 下午
 # @Function:
-
-def getSequence():
+import random
+from Bio import SeqIO
+def getSequenceInturn():
     '''
-    随机获得fasta文件中的sequence
+    顺序获得fasta文件中的sequence
     :return:
     '''
-    with open('/home/data/datacluster/uniclust30_2018_08_fasta/uniclust30_2018_08_seed.fasta') as txt:
+    with open('/home/guo/data/datacluster/uniref50/db/uniref50.fasta') as txt:
         txtData = txt.readlines()
         # print(txtData)
 
     resultFile = open('resultFile.fasta', 'w')
 
     count = 1
-    number = 800
+    number = 50000
     sumLength = 0
 
 
@@ -30,7 +31,52 @@ def getSequence():
     resultFile.close()
     print("sequenceAvaLength = " + str(sumLength/number))
 
-getSequence()
+#getSequenceInturn()
+
+def getSequenceRandom():
+    '''
+    随机获取特定条数的fasta文件里的sequence(未完成
+    :return:
+    '''
+    with open('/home/guo/data/datacluster/uniref50/db/uniref50.fasta')as txt:
+        txtData = txt.readlines()
+    #print(txtData)
+    resultFile = open('resultSequence.fasta','w')
+
+    count = 1
+    number = 5000 # count为所取sequence条数
+
+    while count <= number:
+        count = count + 1
+        randomNumber = random.randint(1,len(txtData))
+        print("len(txtData)为：" + str(len(txtData)))
+        resultFile.write(txtData[randomNumber])
+
+    resultFile.close()
+
+#getSequenceRandom()
+
+def getSequenceInturnBio():
+    '''
+    bio解析+顺序获取特定条数fasta文件里的sequence
+    :return:
+    '''
+
+    count = 1
+    number = 50000
+    resultFile = open('resultBioInturnSequence.fasta','w')
+
+    for seq_record in SeqIO.parse('/home/guo/data/datacluster/uniref50/db/uniref50.fasta','fasta'):
+        count = count + 1
+        if(count < number):
+            record = seq_record
+            resultFile.write('>' + record.description + '\r\n')
+            resultFile.write(str(record.seq) + '\r\n')
+
+    resultFile.close()
+
+getSequenceInturnBio()
+
 
 
 def getSequencefromffdata():
